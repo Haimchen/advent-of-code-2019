ba4b268f09a0c52ba06da13e9a6d58c1d494b90e
Sarah Köhler <sarah.c.koehler@gmail.com>
2019-12-04 23:04:47 +0100

    day 3.2 min delay for cables on grid

--- 3/grid.js
+++ 3/grid.js
@@ -1,48 +1,48 @@
-const parseInstruction = (instruction) => {
+const parseInstruction = instruction => {
   const direction = instruction.charAt(0);
   const steps = Number(instruction.substring(1));
 
-  return { direction, steps }
-}
+  return { direction, steps };
+};
 
 // decrease x value
 const moveLeft = (point, steps) => {
-  const points = []
+  const points = [];
   const [startX, startY] = point;
-  for(let i = 1; i <= steps; i++) {
-    points.push([startX - i, startY])
+  for (let i = 1; i <= steps; i++) {
+    points.push([startX - i, startY]);
   }
   return points;
-}
+};
 
 const moveDown = (point, steps) => {
-  const points = []
+  const points = [];
   const [startX, startY] = point;
-  for(let i = 1; i <= steps; i++) {
-    points.push([startX, startY - i])
+  for (let i = 1; i <= steps; i++) {
+    points.push([startX, startY - i]);
   }
   return points;
-}
+};
 
 const moveUp = (point, steps) => {
-  const points = []
+  const points = [];
   const [startX, startY] = point;
-  for(let i = 1; i <= steps; i++) {
-    points.push([startX, startY + i])
+  for (let i = 1; i <= steps; i++) {
+    points.push([startX, startY + i]);
   }
   return points;
-}
+};
 
 const moveRight = (point, steps) => {
-  const points = []
+  const points = [];
   const [startX, startY] = point;
-  for(let i = 1; i <= steps; i++) {
-    points.push([startX + i, startY])
+  for (let i = 1; i <= steps; i++) {
+    points.push([startX + i, startY]);
   }
   return points;
-}
+};
 const getPointsByInstruction = (currentPoint, direction, steps) => {
-  switch(direction){
+  switch (direction) {
     case "L":
       return moveLeft(currentPoint, steps);
     case "R":
@@ -52,48 +52,78 @@ const getPointsByInstruction = (currentPoint, direction, steps) => {
     case "D":
       return moveDown(currentPoint, steps);
     default:
-      throw new Error(`invalid direction: ${direction}`)
+      throw new Error(`invalid direction: ${direction}`);
   }
-}
+};
 
 // Point: [x, y]
-const getPoints = (directions) => {
-  let points = [[0,0]];
-  const instructions = directions.split(',');
+const getPoints = directions => {
+  let points = [[0, 0]];
+  const instructions = directions.split(",");
   instructions.forEach(instruction => {
     const { direction, steps } = parseInstruction(instruction);
-    const currentPoint = points[points.length - 1]
+    const currentPoint = points[points.length - 1];
     const nextPoints = getPointsByInstruction(currentPoint, direction, steps);
     points = points.concat(nextPoints);
-  })
+  });
   return points;
-}
+};
 
-const getIntersections = (directions1, directions2) => {
-  const path1 = getPoints(directions1)
-  const path2 = getPoints(directions2)
-  const intersections = path1.filter(([x, y]) => path2.findIndex(([x2, y2]) => x == x2 && y == y2) >= 0)
+const getIntersections = (path1, path2) => {
+  const intersections = path1.filter(
+    ([x, y]) => path2.findIndex(([x2, y2]) => x == x2 && y == y2) >= 0
+  );
   return intersections;
-}
+};
+
+const getIntersectionsWithDelay = (path1, path2) => {
+  const intersections = [];
+  path1.forEach(([x, y], index1) => {
+    const index2 = path2.findIndex(([x2, y2]) => x == x2 && y == y2);
+    if (index2 >= 0) {
+      intersections.push({ point: [x, y], index1, index2 });
+    }
+  });
+  return intersections;
+};
 
 const sortNumber = (a, b) => {
   return a - b;
-}
-const getClosestIntersection = (intersections) => {
+};
+
+const getMinDelay = intersections => {
+  const delays = intersections.map(({ index1, index2 }) => index1 + index2);
+  const sortedDelays = delays.sort(sortNumber);
+  return sortedDelays[1];
+};
+
+const getClosestIntersection = intersections => {
   const distances = intersections.map(([x, y]) => Math.abs(x) + Math.abs(y));
-  const minDistance = distances.sort(sortNumber)
+  const minDistance = distances.sort(sortNumber);
   return minDistance[1];
-}
+};
 
 const getClosestDistance = (directions1, directions2) => {
-  const intersections = getIntersections(directions1, directions2);
-  return getClosestIntersection(intersections)
-}
+  const path1 = getPoints(directions1);
+  const path2 = getPoints(directions2);
+  const intersections = getIntersections(path1, path2);
+  return getClosestIntersection(intersections);
+};
 
-const { directions1, directions2 }= require('./input');
-console.log("start calculation of intersections")
-const minDistance = getClosestDistance(directions1, directions2);
-console.log("the minimal distancce is ", minDistance)
+const findMinDelay = (directions1, directions2) => {
+  const path1 = getPoints(directions1);
+  const path2 = getPoints(directions2);
+  const intersections = getIntersectionsWithDelay(path1, path2);
+  return getMinDelay(intersections);
+};
+
+// const { directions1, directions2 } = require("./input");
+// console.log("start calculation of intersections");
+// const minDistance = getClosestDistance(directions1, directions2);
+// console.log("the minimal distance is ", minDistance);
+// console.log("find distance with minimal delay");
+// const minDelay = findMinDelay(directions1, directions2);
+// console.log("the minimal delay is ", minDelay);
 
 module.exports = {
   parseInstruction,
@@ -102,5 +132,5 @@ module.exports = {
   getIntersections,
   getClosestDistance,
   getClosestIntersection,
-}
-
+  getMinDelay
+};
