915bc05ced4af322274210bc66cab5e8a832833c
Sarah Köhler <sarah.c.koehler@gmail.com>
2019-12-05 23:56:29 +0100

    implement alternative check for day 4.2

--- 4/password.js
+++ 4/password.js
@@ -20,10 +20,30 @@ const check = num => {
   return hasDouble && !hasDecrease;
 };
 
-const countValidNumbers = (min, max) => {
+const check2 = num => {
+  const digits = numToArray(num);
+  let hasDouble = false;
+  let hasDecrease = false;
+  let prev = 0;
+  let prevprev = 0;
+  digits.forEach((value, idx) => {
+    const next = digits.length > idx ? digits[idx + 1] : 0;
+    if (value === prev && value != next && value != prevprev) {
+      hasDouble = true;
+    }
+    if (value < prev) {
+      hasDecrease = true;
+    }
+    prevprev = prev;
+    prev = value;
+  });
+  return hasDouble && !hasDecrease;
+};
+
+const countValidNumbers = (min, max, checkFunc) => {
   let count = 0;
   for (let i = min; i <= max; i++) {
-    if (check(i)) {
+    if (checkFunc(i)) {
       count++;
     }
   }
@@ -33,7 +53,7 @@ const countValidNumbers = (min, max) => {
 const min = 172930;
 const max = 683082;
 console.log("counting valid numbers");
-const count = countValidNumbers(min, max);
+const count = countValidNumbers(min, max, check2);
 console.log("valid numbers:", count);
 
-module.exports = { numToArray, check, countValidNumbers };
+module.exports = { numToArray, check, check2, countValidNumbers };
--- 4/password.test.js
+++ 4/password.test.js
@@ -1,4 +1,4 @@
-const { check, countValidNumbers, numToArray } = require("./password");
+const { check, check2, countValidNumbers, numToArray } = require("./password");
 
 test("digit is parsed to array containing digit", () => {
   expect(numToArray(1)).toEqual([1]);
@@ -13,17 +13,49 @@ test("check returns true for 111111", () => {
 });
 
 test("check returns true for 123455", () => {
-  expect(check(111111)).toEqual(true);
+  expect(check(123455)).toEqual(true);
 });
 
 test("check returns false for 223450", () => {
   expect(check(223450)).toEqual(false);
 });
 
-test("range of one valid number is counted correctly", () => {
-  expect(countValidNumbers(111111, 111111)).toEqual(1);
+test("check2 returns true for 123455", () => {
+  expect(check2(123455)).toEqual(true);
 });
 
-test("range of two valid number is counted correctly", () => {
-  expect(countValidNumbers(11, 22)).toEqual(2);
+test("check2 returns true for 111455", () => {
+  expect(check2(111455)).toEqual(true);
+});
+
+test("check2 returns false for 123444", () => {
+  expect(check2(123444)).toEqual(false);
+});
+
+test("check2 returns false for 444444", () => {
+  expect(check2(444444)).toEqual(false);
+});
+
+test("check2 returns true for 223344", () => {
+  expect(check2(223344)).toEqual(true);
+});
+
+test("check2 returns false for 222345", () => {
+  expect(check2(222345)).toEqual(false);
+});
+
+test("check2 returns false for 123444", () => {
+  expect(check2(123444)).toEqual(false);
+});
+
+test("check2 returns true for 224444", () => {
+  expect(check2(224444)).toEqual(true);
+});
+
+test("range of one valid number is counted correctly with check", () => {
+  expect(countValidNumbers(111111, 111111, check)).toEqual(1);
+});
+
+test("range of two valid number is counted correctly with check", () => {
+  expect(countValidNumbers(11, 22, check)).toEqual(2);
 });
